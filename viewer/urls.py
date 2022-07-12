c419a046f7a3b2ae9a920c128959f4f29e548f57
hugh-braico <15304925+hugh-braico@users.noreply.github.com>
2022-07-12 15:06:39 +1000

    Put up interim dump of the old TWB database

--- viewer/urls.py
+++ viewer/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from . import views
 
@@ -7,5 +8,6 @@ app_name = 'viewer'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
-    path('backup.csv', views.csv, name='csv')
+    path('backup.csv', views.csv, name='csv'),
+    path('old_twb_dump.csv', TemplateView.as_view(template_name='viewer/old_twb_dump.csv', content_type='text/plain'))
 ]
