469b88900520c67ad711528b1a72d786f300fc1e
hugh-braico <15304925+hugh-braico@users.noreply.github.com>
2023-01-21 12:02:46 +1100

    chore: don't censor Skullgirls OCE in event names any more, since we have some global data now

--- viewer/models.py
+++ viewer/models.py
@@ -119,7 +119,6 @@ class Vod(models.Model):
     # (Called whenever the event name changes during the loop)
     def table_header_html(self):
         formatted_date = self.date.strftime('%d %b %Y')
-        event = self.event.replace("Skullgirls OCE ", "")
         return f"<tr class=\"event\">" \
                f"<th colspan=\"4\" class=\"name\">{event}</th>" \
                f"<th class=\"date\">{formatted_date}</th>" \
